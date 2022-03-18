1a2792c64f4a88204da5f1e6a3223c846f5e6f14
joedietrich-dev <joe@joedietrich.net>
2022-03-17 21:09:32 -0400

    Create routes for sessions and registrations

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,5 +310,12 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 7.days.to_i
   end
 end
