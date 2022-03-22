29b4973f4918274e8539d16f56ac39fbebbcf6ff
joedietrich-dev <joe@joedietrich.net>
2022-03-21 21:49:28 -0400

    Edit serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :full_name, :created_at
+  set_id :organization_id
+  set_id :role_id
+  attributes :id, :email, :full_name, :created_at, :organization_id, :role_id
+  belongs_to :organization
+  belongs_to :role
 end
