e07f4fa18ac01fb73c65a318a8e62b910d4760dd
joedietrich-dev <joe@joedietrich.net>
2022-04-16 15:34:05 -0400

    add logo url to organization json

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -1,7 +1,7 @@
 class OrganizationSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :name, :albums
+  attributes :id, :name, :albums, :logo_url
   attribute :users, serializer: UserSerializer
   has_many :users
   has_many :albums
